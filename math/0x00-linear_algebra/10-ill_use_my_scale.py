3ed16f79f5e07d861e049f00ea6fc289b89711f4
AndrewMiranda <40378450+AndrewMiranda@users.noreply.github.com>
2021-07-16 00:09:19 -0500

    10ILL

---
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module used to get the shape of a matrix
+"""
+
+
+def np_shape(matrix):
+    """Matrix add function
+    """
+    import numpy as np
+
+    return np.shape(np.array(matrix))
