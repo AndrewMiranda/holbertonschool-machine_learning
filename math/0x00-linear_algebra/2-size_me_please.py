9f53dfa226a035b15e3199bcf1235aaab24067b7
Andrew Miranda <tiburonandrew@example.com>
2021-07-16 01:18:18 +0000

    2-SizeMe

---
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Calculate the shape of a matrix
+"""
+
+def matrix_shape(matrix):
+    if type(matrix[0]) is not list:
+        return [len(matrix)]
+    else:
+        return [len(matrix)] + matrix_shape(matrix[0])
