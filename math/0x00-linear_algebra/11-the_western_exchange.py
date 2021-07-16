ac3a5cf905c95c37add8613c8d56ae07281cd929
AndrewMiranda <40378450+AndrewMiranda@users.noreply.github.com>
2021-07-16 00:10:13 -0500

    11TheWestern

---
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Module used to get the transpose of a matrix
+"""
+
+
+def np_transpose(matrix):
+    """Matrix add function
+    """
+    import numpy as np
+
+    return np.transpose(np.array(matrix))
+  
