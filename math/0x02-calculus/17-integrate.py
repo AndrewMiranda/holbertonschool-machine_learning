1532f83f230836fe0f5fe382d24f6609444269b4
AndrewMiranda <40378450+AndrewMiranda@users.noreply.github.com>
2021-07-26 12:07:26 -0500

    upt

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -1,26 +1,24 @@
-""" Integration """
+#!/usr/bin/env python3
+"""Function to calculate the integral"""
 
 
 def poly_integral(poly, C=0):
-    """
-    INPUT: polynomial
-    OUTPUT integration polynomial
-    """
-    if type(poly) is not list or len(poly) == 0:
+    """Function calculating the integral"""
+    new_L = []
+    i = 0
+
+    if type(poly) != list or len(poly) == 0 or type(C) != int:
         return None
-    if type(C) is not int and type(C) is not float:
-        return None
-    if type(poly[0]) is not int and type(poly[0]) is not float:
-        return None
-    flag = True
-    aux = []
-    for i in range(len(poly) - 1, -1, -1):
-        if poly[i] == 0 and flag:
-            pass
+    new_L.append(C)
+    if sum(poly) == 0:
+        return new_L
+    if len(poly) == 1:
+        new_L.append(poly[0])
+        return new_L
+    while i < len(poly):
+        if poly[i] % (i + 1) == 0:
+            new_L.append(int(poly[i]/(i + 1)))
         else:
-            aux.append(poly[i])
-            flag = False
-    aux.reverse()
-    integration = [C] + [aux[i] / (i + 1) for i in range(len(aux))]
-    integration = [int(x) if x % 1 == 0 else x for x in integration]
-    return integration
+            new_L.append(poly[i]/(i + 1))
+        i += 1
+    return new_L
