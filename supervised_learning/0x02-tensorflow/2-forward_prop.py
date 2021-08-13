fb00aea5884eadd63e8ee619986fd3802ce96cac
AndrewMiranda <40378450+AndrewMiranda@users.noreply.github.com>
2021-08-12 21:31:58 -0500

    Task 2
    Sequence create

---
+++ supervised_learning/0x02-tensorflow/2-forward_prop.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Module for the function forward_prop
+"""
+
+import tensorflow as tf
+create_layer = __import__('1-create_layer').create_layer
+
+
+def forward_prop(x, layer_sizes=[], activations=[]):
+    """creates the forward propagation graph for the neural network
+    Args:
+        x: The placeholder for the input data
+        layer_sizes: A list containing the number of nodes in each layer of
+        the network
+        activations: A list containing the activation functions for each layer
+        of the network
+    Returns:
+        The prediction of the network in tensor form
+    """
+
+    layers = create_layer(x, layer_sizes[0], activations[0])
+    for idx in range(1, len(layer_sizes)):
+        layers = create_layer(layers, layer_sizes[idx], activations[idx])
+    return layers
