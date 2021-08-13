4f9388fee8dc908ba62cb8aba8e7dd809884bc06
AndrewMiranda <40378450+AndrewMiranda@users.noreply.github.com>
2021-08-12 21:30:15 -0500

    1CreateLayer

---
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+'''Module for the function create_layer
+'''
+
+import tensorflow as tf
+
+
+def create_layer(prev, n, activation):
+    '''Createa a tensor layer
+    Args:
+        prev: The tensor output of the previous layer
+        n: The number of nodes in the layer to create
+        activation: The activation function that the layer should use
+    '''
+
+    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    x = tf.layers.Dense(units=n,
+                        activation=activation,
+                        name='layer',
+                        kernel_initializer=init)(prev)
+    return x
