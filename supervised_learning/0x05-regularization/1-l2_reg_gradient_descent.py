a5a976b5beafc20792ed3ac1ffdff1b1520faf7e
AndrewMiranda <40378450+AndrewMiranda@users.noreply.github.com>
2021-08-31 23:37:53 -0500

    Fix

--- supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -1,43 +1,26 @@
 #!/usr/bin/env python3
-'''Module for the function
-l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L)
-'''
-
+"""Gradient Descent with L2 Regularization"""
 import numpy as np
 
 
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
-    '''Updates the weights and biases of a neural network using
-    gradient descent with L2 regularization (The neural network
-    uses tanh activations on each layer except the last, which
-    uses a softmax activation, the weights and biases of the
-    network are updated in place).
-    
-    Args.
-        Y: A one-hot numpy.ndarray of shape (classes, m) that contains the
-        correct labels for the data.
-            - classes: The number of classes.
-            - m: The number of data points.
-        weights: A dictionary of the weights and biases of the NN.
-        cache: A dictionary of the outputs of each layer of the NN.
-        alpha: The learning rate.
-        lambtha: The L2 regularization parameter.
-        L: The number of layers of the network.
-    '''
-
-    m = Y.shape[1]
-    dz = cache["A{}".format(L)] - Y
+    """function that updates the weights and biases of a nn using
+        gradient descent with L2 regularization"""
+    weights_copy = weights.copy()
     for i in range(L, 0, -1):
-        # frobenius norm
-        f_norm = (lambtha / m) * weights["W{}".format(i)]
+        m = Y.shape[1]
+        if i != L:
+            # all layers use a tanh activation, except last
+            # introduce call to tanh_prime method
+            dZi = np.multiply(np.matmul(
+                weights_copy['W' + str(i + 1)].T, dZi
+            ), 1 - cache['A' + str(i)] ** 2)
+        else:
+            # last layer uses a softmax activation
+            dZi = cache['A' + str(i)] - Y
+        dWi = np.matmul(dZi, cache['A' + str(i - 1)].T) / m
+        dbi = np.sum(dZi, axis=1, keepdims=True) / m
 
-        # calculate dw and db, replace values of W and b
-        dw_i = ((1 / m) * np.matmul(dz, cache["A{}".format(i - 1)].T)) + f_norm
-        db_i = (1 / m) * np.sum(dz, axis=1, keepdims=True)
-        weights["W{}".format(i)] -= (alpha * dw_i)
-        weights["b{}".format(i)] -= (alpha * db_i)
-
-        # define dz for next epoch
-        dz_1 = np.matmul(weights["W{}".format(i)].T, dz)
-        dz_af = 1 - np.square(cache["A{}".format(i - 1)])
-        dz = dz_1 * dz_af
+        l2 = (1 - alpha * lambtha / m)
+        weights['W' + str(i)] = l2 * weights_copy['W' + str(i)] - alpha * dWi
+        weights['b' + str(i)] = weights_copy['b' + str(i)] - alpha * dbi
