55dd4ade82010f61a6b2942d483c62e314e1b759
AndrewMiranda <40378450+AndrewMiranda@users.noreply.github.com>
2021-09-14 20:03:21 -0500

    12Test

--- supervised_learning/0x06-keras/12-test.py
+++ supervised_learning/0x06-keras/12-test.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
-"""Test"""
-import tensorflow.keras as K
+'''Test module'''
+import tensorflow.keras as Keras
 
 
 def test_model(network, data, labels, verbose=True):
-    """function that tests a neural network"""
-
-    return network.evaluate(data, labels, verbose=verbose)
+    '''tests a neural network
+    Args:
+        network is the network model to test
+        data is the input data to test the model with
+        labels are the correct one-hot labels of data
+        verbose is a boolean that determines if output should be printed
+                during the testing process
+    Returns: the loss and accuracy of the model with the testing data,
+             respectively
+    '''
+    return network.evaluate(x=data,
+                            y=labels,
+                            verbose=verbose)
