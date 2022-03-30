77e5e09277a8c696196006ee2852799dce14ebea
AndrewMiranda <40378450+AndrewMiranda@users.noreply.github.com>
2022-03-30 18:10:32 -0500

    Fix 6

--- unsupervised_learning/0x00-dimensionality_reduction/6-grads.py
+++ unsupervised_learning/0x00-dimensionality_reduction/6-grads.py
@@ -2,26 +2,33 @@
 """Function that calculates the gradients of Y"""
 
 import numpy as np
-
-
 Q_affinities = __import__('5-Q_affinities').Q_affinities
 
 
 def grads(Y, P):
-    """Y is a numpy.ndarray of shape (n, ndim)
-    containing the low dimensional transformation of X
-    P is a numpy.ndarray of shape (n, n)
-    containing the P affinities of X
-    Do not multiply the gradients by the scalar
-    4 as described in the paper’s equation
+    """
+    Calculate the gradients of Y
+    Args:
+        Y: numpy.ndarray of shape (n, ndim) containing the low
+           dimensional transformation of X
+        P: numpy.ndarray of shape (n, n) containing the P
+           affinities of X
     Returns: (dY, Q)
-        dY is a numpy.ndarray of shape (n, ndim) containing the gradients of Y
-        Q is a numpy.ndarray of shape (n, n) containing the Q affinities of Y
-    You may use Q_affinities = __import__('5-Q_affinities').Q_affinities"""
+             dY: dY is a numpy.ndarray of shape (n, ndim) containing the
+             gradients of Y
+             Q: Q is a numpy.ndarray of shape (n, n) containing the Q
+                affinities of Y
+    """
+    n, ndim = Y.shape
     Q, num = Q_affinities(Y)
-    P_Q = P - Q
-    dY = np.zeros(Y.shape)
-    for i in range(Y.shape[0]):
-        dY[i, :] = np.sum(np.tile(P_Q[:, i] * num[:, i],
-                                  (Y.shape[1], 1)).T * (Y[i, :] - Y), 0)
-    return (dY, Q)
+    dY = np.zeros((n, ndim))
+
+    PQ = P - Q
+    # y.T = y.T-n − n dC/dYi
+    PQ_expanded = np.expand_dims((PQ * num).T, axis=2)
+
+    for i in range(n):
+        y_diff = Y[i, :] - Y
+        # dC / dY[i, :] = 4. * (sum((pij - qij) * (yi - yj))
+        dY[i, :] = np.sum((PQ_expanded[i, :] * y_diff), 0)
+    return dY, Q
