87321d7c91fa38c1b41b4a5b9959c72c4dd254df
AndrewMiranda <40378450+AndrewMiranda@users.noreply.github.com>
2022-09-14 12:14:51 -0500

    Fix 20

--- pipeline/0x02-databases/20-average_score.sql
+++ pipeline/0x02-databases/20-average_score.sql
@@ -1,11 +1,11 @@
--- stored procedure that computes average given a student id
-DELIMITER $$
-DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+-- Script that creates a stored procedure ComputeAverageScoreForUser
+-- that computes and store the avg score for a student
+delimiter $$
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
     UPDATE users
-    SET users.average_score = AVG(corrections.score) WHERE users.id = user_id;
-
-END;
-$$
-DELIMITER ;
+        SET average_score = (
+            SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
+        WHERE id = user_id;
+END $$
+delimiter ;
