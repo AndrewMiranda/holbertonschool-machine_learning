8ec8ebcccd2dc1fdcf40d0c5eddd3b33c4fb9fba
AndrewMiranda <40378450+AndrewMiranda@users.noreply.github.com>
2022-09-14 12:16:44 -0500

    Fix 21

--- pipeline/0x02-databases/21-div.sql
+++ pipeline/0x02-databases/21-div.sql
@@ -1,20 +1,8 @@
-DELIMITER $$
-DROP FUNCTION IF EXISTS SafeDiv;
-CREATE FUNCTION SafeDiv (f_1 FLOAT, f_2 FLOAT)
-RETURNS FLOAT
-DETERMINISTIC
-BEGIN
-    DECLARE answer FLOAT;
-
-    IF f_2 = 0 THEN
-        SET answer = 0;
-    ELSEIF IFNULL(f_2, 0) THEN
-        SET answer = 0;
-    ELSE
-        SET answer = f_1 / f_2;
-    END IF;
-
-    RETURN(answer);
-END;
-$$
-DELIMITER ;
+-- function that safely divides
+delimiter $$
+CREATE FUNCTION SafeDiv(a INT, b INT)
+	RETURNS FLOAT
+	BEGIN
+		RETURN (IF (b = 0, 0, a / b));
+	END $$
+delimiter ;
